c112ffb77a30106393ab2b6c807bb9b23ea66bab
efcjunior <everson.c.junior@gmail.com>
2021-11-10 21:00:03 -0300

    add transaction pool

--- app/index.js
+++ app/index.js
@@ -1,16 +1,28 @@
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
 const express = require('express');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
-const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
 
 app.use(express.json());
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+app.get('/public-key', (req, res) => {
+    res.json({publicKey: wallet.publicKey});
 });
 
 app.post('/mine', (req, res) => {
@@ -20,6 +32,12 @@ app.post('/mine', (req, res) => {
     res.redirect('/blocks');
 });
 
-app.listen(HTTP_PORT, console.log(`APP is listening on HTTP PORT ${HTTP_PORT}`));
+app.post('/transact', (req, res) => {
+    const {recipient, amount} = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    p2pServer.broadcastTransaction(transaction);
+    res.redirect('/transactions');
+});
 
+app.listen(HTTP_PORT, console.log(`APP is listening on HTTP PORT ${HTTP_PORT}`));
 p2pServer.listen();
