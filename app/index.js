2272f2a7c6a00b60aa3de4ea589fad232fded09a
efcjunior <everson.c.junior@gmail.com>
2021-11-10 21:01:23 -0300

    fix bug

--- app/index.js
+++ app/index.js
@@ -21,7 +21,7 @@ app.get('/transactions', (req, res) => {
     res.json(tp.transactions);
 });
 
-app.get('git', (req, res) => {
+app.get('/public-key', (req, res) => {
     res.json({publicKey: wallet.publicKey});
 });
 
